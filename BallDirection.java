82179b12e17a4fa91d4d43d5381e0b36b1dac738
Palakaaa <56376957+Palakaaa@users.noreply.github.com>
2019-10-09 22:16:59 -0500

    hw1
    heree ya go

---
+++ BallDirection.java
@@ -0,0 +1,13 @@
+package hw2;
+
+/**
+ * @author Brad Warren: bawarren@iastate.edu
+ */
+public enum BallDirection {
+	/**
+	 * Constants representing the direction in which the ball is traveling in an
+	 * instance of {@link TennisGame}
+	 */
+
+	NOT_IN_PLAY, TOWARD_RECEIVER, TOWARD_SERVER;
+}
